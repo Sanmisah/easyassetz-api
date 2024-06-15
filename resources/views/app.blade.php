fd9011e9fdb3e35a10779825f16886c93dc989e4
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-06-15 13:09:02 +0530

    commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('./assets/index-BcQpi0QZ.css') }}">
+    <link rel="stylesheet" href="{{ asset('./assets/index-Dzdmmsem.css') }}">
 </head>
 <body>
     <div id="root"></div>
-    <script src="{{ asset('./assets/index-CwuTDuAB.js') }}"></script>
+    <script src="{{ asset('./assets/index-De_VtAWQ.js') }}"></script>
 </body>
 </html>
