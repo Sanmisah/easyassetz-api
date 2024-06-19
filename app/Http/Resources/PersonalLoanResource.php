f8da0bf8590530dba7d1ca78e94a77ca93ce95d6
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-06-19 19:11:45 +0530

    htght

--- app/Http/Resources/PersonalLoanResource.php
+++ app/Http/Resources/PersonalLoanResource.php
@@ -14,6 +14,19 @@ class PersonalLoanResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
-    }
+        return [
+            'id' => $this->id,
+            'profileId' => $this->profile_id,
+            'bankName' => $this->bank_name,
+            'loanAccountNo' => $this->loan_account_no,
+            'branch' => $this->branch,
+            'emiDate' => $this->emi_date,
+            'startDate' => $this->start_date,
+            'duration' => $this->duration,
+            'guarantorName' => $this->guarantor_name,
+            'guarantorMobile' => $this->guarantor_mobile,
+            'guarantorEmail' => $this->guarantor_email,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];    }
 }
