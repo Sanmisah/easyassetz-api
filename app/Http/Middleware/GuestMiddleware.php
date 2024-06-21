8bb24b1df59ee4d9788dc566d893c3f091312b6b
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-06-21 18:50:12 +0530

    gfr

--- app/Http/Middleware/GuestMiddleware.php
+++ app/Http/Middleware/GuestMiddleware.php
@@ -20,35 +20,15 @@ class GuestMiddleware extends BaseController
     public function handle(Request $request, Closure $next): Response
     {
        
-        // $user = Auth::user();
-        // if($user){
+        $user = Auth::user();
+         if($user){
         //     $token = PersonalAccessToken::where('tokenable_id', $user->id)
         //     ->where('tokenable_type', get_class($user))
         //     ->first();    
         // return $this->sendResponse(['user'=>new UserResource($user), 'token'=>$token->plainTextToken], 'User login successfully.');           
 
-        // //  return $this->sendError('Unauthorised.', ['error'=>'You are already logged-in']);
-        // }
-
-        $user = auth()->user();
-
-if ($user) {
-    $token = PersonalAccessToken::where('tokenable_id', $user->id)
-                                ->where('tokenable_type', get_class($user))
-                                ->first();
-   // $token = $user->currentAccessToken();
-
-    if ($token) {
-        return $this->sendResponse([
-            'user' => new UserResource($user),
-            'token' => $token->plainTextToken,
-        ], 'User login successfully.');
-    } else {
-        // Handle case where no token is found (though it shouldn't happen if user is authenticated)
-        return $this->sendError('Token not found.', ['error' => 'Token for user not found']);
-    }
-}
-
+          return $this->sendError('Unauthorised.', ['error'=>'You are already logged-in']);
+        }
         return $next($request);
     }
 }
