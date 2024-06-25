2d272a499432de70cbf46258e45325431f78f926
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-06-25 16:42:11 +0530

    th

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('{any}', function () {
     // return view('app');
-   // return file_get_contents(public_path('index.html'));
-   return file_get_contents('index.html');
+    return file_get_contents(public_path('index.html'));
+  // return file_get_contents('index.html');
 
 })->where('any', '.*');
