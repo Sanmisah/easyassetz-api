a21ccc1e184068cb190e38d712cb0eab8a4eba6c
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-07-19 16:46:10 +0530

    htt

--- app/Models/OtherDeposite.php
+++ app/Models/OtherDeposite.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Beneficiary;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OtherDeposite extends Model
 {
     use HasFactory;
+
+    public function nominee(){
+        return $this->belongsToMany(Beneficiary::class,'other_financial_nominee');
+     }
+
+     public function jointHolder(){
+        return $this->belongsToMany(Beneficiary::class,'other_financial_joint_holder');
+     }
 }
