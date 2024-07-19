8f7bbf06188881ed3c2b7969ab77897d69b0745d
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-07-19 17:57:42 +0530

    rgvfr

--- app/Models/PostSavingScheme.php
+++ app/Models/PostSavingScheme.php
@@ -10,6 +10,17 @@ class PostSavingScheme extends Model
 {
     use HasFactory;
 
+    public function setMaturityDateAttribute($value)
+    {
+            $this->attributes['maturity_date'] = $value ? Carbon::createFromFormat('m/d/Y', $value)->format('Y-m-d') : null;
+    }
+
+    public function getMaturityDateAttribute($value)
+    {
+            return $value ? Carbon::parse($value)->format('m/d/Y') : null;   
+    }    
+
+
     public function nominee(){
         return $this->belongsToMany(Beneficiary::class,'post_saving_scheme_nominee');
      }
