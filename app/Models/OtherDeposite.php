300a871bc4f528e3b9ffe55d71831e90fb20badd
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-07-22 11:24:48 +0530

    grfgtf

--- app/Models/OtherDeposite.php
+++ app/Models/OtherDeposite.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use App\Models\Beneficiary;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -17,4 +18,18 @@ class OtherDeposite extends Model
      public function jointHolder(){
         return $this->belongsToMany(Beneficiary::class,'other_financial_joint_holder');
      }
+
+     public function setMaturityDateAttribute($value)
+     {
+         if($value === "null") {
+             $value = null;
+         }
+         $this->attributes['maturity_date'] = $value ? Carbon::createFromFormat('m/d/Y', $value)->format('Y-m-d') : null;   
+     }
+ 
+     public function getMaturityDateAttribute($value)
+     {
+             return $value ? Carbon::parse($value)->format('m/d/Y') : null;
+     }    
+
 }
